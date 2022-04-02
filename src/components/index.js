f7da30db8d1de0f50e6dd1f21f02dcac4e6d7560
asphyxiadeath <86077497+asphyxiadeath@users.noreply.github.com>
2022-04-02 12:18:10 +0300

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as AddList} from "./addListButton/AddList";
+export {default as Bagde} from './Badge/badge';
+export {default as List} from './List';
+export {default as Tasks} from './tasks/Tasks';
